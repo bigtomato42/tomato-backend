e6b7ddcf97ca13b5bd2dc2fbee2172849339e2fa
jakub <JakubyKriz@gmail.com>
2019-05-03 19:58:21 +0200

    debug=false

--- conf/settings/production.py
+++ conf/settings/production.py
@@ -5,7 +5,6 @@ from .base import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -14,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
