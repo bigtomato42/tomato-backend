3d8d120651dc5e83fcc70fcaf3a187fcbc1cc1b0
jakub <JakubyKriz@gmail.com>
2019-05-03 20:08:46 +0200

    changed allowed hosts

--- conf/settings/production.py
+++ conf/settings/production.py
@@ -15,7 +15,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://bigtomato.herokuapp.com/']
 
 WSGI_APPLICATION = 'conf.wsgi.application'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
