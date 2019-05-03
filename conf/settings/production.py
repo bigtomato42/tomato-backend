3e3f6703d873a0cd955e8c156a55b77c0915fa47
jakub <JakubyKriz@gmail.com>
2019-05-03 19:56:14 +0200

    changed static

--- conf/settings/production.py
+++ conf/settings/production.py
@@ -5,7 +5,7 @@ from .base import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
