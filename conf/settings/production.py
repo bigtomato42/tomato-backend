501fd153245e3711f8f3100c9d214c5dd9188030
jakub <JakubyKriz@gmail.com>
2019-05-09 23:10:28 +0200

    debugTrue

--- conf/settings/production.py
+++ conf/settings/production.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['bigtomato.herokuapp.com']
 
