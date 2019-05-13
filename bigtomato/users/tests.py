59f00a4cd23f4bd028d0eea95ec86be2cc5fd5c8
jakub <JakubyKriz@gmail.com>
2019-05-13 09:45:42 +0200

    test + circleci setup

--- bigtomato/users/tests.py
+++ bigtomato/users/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+class RandomTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_stuff(self):
+        print("works !")
+        return True
