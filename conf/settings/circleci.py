e2c1d30d4619f1e4e6a880fc079603df1235c278
jakub <JakubyKriz@gmail.com>
2019-05-14 22:30:44 +0200

    databaseurl for circleci

---
+++ conf/settings/circleci.py
@@ -0,0 +1,38 @@
+import os
+import django_heroku
+from .base import *
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+
+
+SECRET_KEY = 'nvk3(06snl(z=6xw6--)4%3l=%4jpptiiy006&zf$%j73waqhi'
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '..', 'static'),
+)
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+    'default': os.environ['POSTGRES_DB']
+}
+
+
+
+STATIC_URL = '/static/'
